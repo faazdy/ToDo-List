dc7dbf4269506e7f9cf0682b4cdec1be4d480d3c
faazdy <leorengifor@gmail.com>
2024-08-02 20:06:37 -0500

    -m 'buttons with icons from JS'

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
         e.preventDefault();
 
         const taskGroup = document.querySelector('#task-container')
-        const task = document.createElement('li') 
+        const task = document.createElement('li')
+        const check = document.createElement('input')
+        check.type = 'checkbox' 
+        
+        
+        const anchor = document.createElement('button')
+        const deleteBtn = document.createElement('button')
+        //funcion para añadir los iconos
+        function addIcon(i){
+            const icon = document.createElement('img')
+            icon.src = i;
+            return icon;
+        }
+        anchor.append(addIcon('ss'))
+        deleteBtn.append(addIcon('as'))
+
         taskGroup.appendChild(task)
         task.innerText = input.value
+        task.append(check)
+        task.append(anchor)
+        task.append(deleteBtn)
+
+        input.value = ''
     })
 })
